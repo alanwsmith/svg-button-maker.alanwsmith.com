8f495887cf3f56dc2fea2fc7fb9d8cdcc2640a47
Alan Smith <commit@alanwsmith.com>
2025-01-14 12:30:56 -0500

    Switched to using page variables for button colors

--- site/scripts/svg-to-css-converter.js
+++ site/scripts/svg-to-css-converter.js
@@ -43,26 +43,30 @@ ${state.buttonSelector.value} ${state.childSelector.value} {
 
 function doUpdate() {
   let results = convert(state.svgInput.value)
-  state.exampleWrapper.innerHTML = state.buttonHTML.value
   state.cssOutput.value = results
   state.stylesheet.textContent = results
 }
 
 function loadInitialValues() {
-  const svgInput = `<?xml version="1.0" encoding="UTF-8"?><svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000" stroke-width="0.5"><path d="M6.90588 4.53682C6.50592 4.2998 6 4.58808 6 5.05299V18.947C6 19.4119 6.50592 19.7002 6.90588 19.4632L18.629 12.5162C19.0211 12.2838 19.0211 11.7162 18.629 11.4838L6.90588 4.53682Z" fill="#000000" stroke="#000000" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>`
-  state.svgInput.value = svgInput
-  const buttonHTML = `<button class="play-button"><div><div></button>`
-  state.buttonHTML.value = buttonHTML 
+  state.svgInput.value = `<?xml version="1.0" encoding="UTF-8"?><svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000" stroke-width="0.5"><path d="M6.90588 4.53682C6.50592 4.2998 6 4.58808 6 5.05299V18.947C6 19.4119 6.50592 19.7002 6.90588 19.4632L18.629 12.5162C19.0211 12.2838 19.0211 11.7162 18.629 11.4838L6.90588 4.53682Z" fill="#000000" stroke="#000000" stroke-width="0.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>`
+  state.buttonHTML.value = `<button class="play-button"><div><div></button>`
   state.buttonSelector.value = '.play-button'
   state.childSelector.value = 'div'
-  state.buttonWidth.value = '2rem'
+  state.buttonWidth.value = '3.5rem'
   state.buttonHeight.value = '2rem'
-  state.borderColorValue.value = 'white'
-  state.borderColorVar.value = '--button-border-color'
-  state.buttonColorValue.value = 'goldenrod'
+  state.buttonColorValue.value = 'var(--accent-color-1)'
   state.buttonColorVar.value = '--button-color'
-  state.backgroundColorValue.value = 'purple'
+  state.borderColorValue.value = 'var(--reverse-color)'
+  state.borderColorVar.value = '--button-border-color'
+  state.backgroundColorValue.value = 'var(--accent-color-2)'
   state.backgroundColorVar.value = '--button-background-color'
+  const exampleWrapperNodes = document.querySelectorAll(".exampleWrapper")
+  state.exampleWrappers = [...exampleWrapperNodes]
+  state.exampleWrappers.forEach((exampleWrapper) => {
+    console.log(exampleWrapper)
+    console.log(state.buttonHTML.value)
+    exampleWrapper.innerHTML = state.buttonHTML.value
+  })
 }
 
 function prepElements() {
@@ -78,7 +82,6 @@ function prepElements() {
     "buttonWidth",
     "childSelector",
     "cssOutput",
-    "exampleWrapper",
     "svgInput",
     "buttonHTML",
   ]
